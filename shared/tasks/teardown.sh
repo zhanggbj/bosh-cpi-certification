3baeeba0a42595e9527dcb5515e9ef395f26fd57
caleb miles <cmiles@pivotal.io>
2016-03-15 09:49:37 -0700

    Update shared tasks to make flow of inputs and outputs more clear
    [#111543866](https://www.pivotaltracker.com/story/show/111543866)
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- shared/tasks/teardown.sh
+++ shared/tasks/teardown.sh
@@ -2,36 +2,37 @@
 
 set -e
 
-source this/shared/utils.sh
+source pipelines/shared/utils.sh
 source /etc/profile.d/chruby.sh
 chruby 2.1.7
 
-# TODO: make this IaaS-shareable
-: ${BOSH_DIRECTOR_USERNAME:?}
-: ${BOSH_DIRECTOR_PASSWORD:?}
-: ${DEPLOYMENT_NAME:?}
-
 # preparation
-cp ./director-state/director{.*,-state.json} .
 cp -r director-state/.bosh_init $HOME/
-
-# configuration
-source director.env
-: ${DIRECTOR_IP:?}
-
-# teardown deployments against BOSH Director
-time bosh -n target ${DIRECTOR_IP}
-time bosh login ${BOSH_DIRECTOR_USERNAME} ${BOSH_DIRECTOR_PASSWORD}
-
-time bosh -n delete deployment ${DEPLOYMENT_NAME} --force
-time bosh -n cleanup --all
-
-# teardown BOSH Director
 bosh_init=$(realpath bosh-init/bosh-init-*)
 chmod +x $bosh_init
+
+echo "using bosh CLI version..."
+bosh -v
 
 echo "using bosh-init CLI version..."
 $bosh_init version
 
-echo "deleting existing BOSH Director VM..."
-$bosh_init delete director.yml
+pushd director-state > /dev/null
+  # configuration
+  source director.env
+  : ${BOSH_DIRECTOR_IP:?}
+  : ${BOSH_DIRECTOR_USERNAME:?}
+  : ${BOSH_DIRECTOR_PASSWORD:?}
+
+  # teardown deployments against BOSH Director
+  time bosh -n target ${BOSH_DIRECTOR_IP}
+  time bosh login ${BOSH_DIRECTOR_USERNAME} ${BOSH_DIRECTOR_PASSWORD}
+
+  if [ -n "${DEPLOYMENT_NAME}" ]; then
+    time bosh -n delete deployment ${DEPLOYMENT_NAME} --force
+  fi
+  time bosh -n cleanup --all
+
+  echo "deleting existing BOSH Director VM..."
+  $bosh_init delete director.yml
+popd
