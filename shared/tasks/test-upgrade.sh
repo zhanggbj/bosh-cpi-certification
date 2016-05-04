ed86ef7c34735ea8e6e86f46ef280d558a39a642
Corey Innis <cinnis@pivotal.io>
2016-05-04 11:21:45 -0700

    Fix missing resource error in upgrade job
    - There were some required inputs (e.g. shared.pem) that were missing in
      the upgrade job.
    - This commit makes the upgrade job look more like deploy-director so
      that resources are properly copied into the output dir
    
    [#116374055](https://www.pivotaltracker.com/story/show/116374055)
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- shared/tasks/test-upgrade.sh
+++ shared/tasks/test-upgrade.sh
@@ -5,35 +5,53 @@ set -e -x
 source /etc/profile.d/chruby.sh
 chruby 2.1.7
 
-# inputs
 : ${DEPLOYMENT_NAME:?}
 
-director_config=$(realpath new-director-config/)
-director_state=$(realpath director-state/)
-source ${director_config}/director.env
-: ${BOSH_DIRECTOR_IP:?}
-: ${BOSH_DIRECTOR_USERNAME:?}
-: ${BOSH_DIRECTOR_PASSWORD:?}
+# inputs
+existing_director_config=$(realpath director-config/)
+existing_director_state=$(realpath director-state/)
+stemcell_dir=$(realpath stemcell/)
+bosh_dir=$(realpath bosh-release/)
+cpi_dir=$(realpath cpi-release/)
 
 # outputs
 output_dir=$(realpath new-director-state/)
 
-cp ${director_config}/director.yml .
-cp ${director_state}/director-state.json .
+source ${existing_director_config}/director.env
+: ${BOSH_DIRECTOR_IP:?}
+: ${BOSH_DIRECTOR_USERNAME:?}
+: ${BOSH_DIRECTOR_PASSWORD:?}
 
-initver=$(cat bosh-init/version)
-initexe="bosh-init/bosh-init-${initver}-linux-amd64"
-chmod +x ${initexe}
+cp ${existing_director_config}/* ${output_dir}
+cp ${existing_director_state}/* ${output_dir}
+
+# deployment manifest references releases and stemcells relative to itself...make it true
+# these resources are also used in the teardown step
+mkdir -p ${output_dir}/{stemcell,bosh-release,cpi-release}
+cp ${stemcell_dir}/*.tgz ${output_dir}/stemcell/
+cp ${bosh_dir}/*.tgz ${output_dir}/bosh-release/
+cp ${cpi_dir}/*.tgz ${output_dir}/cpi-release/
+
+bosh_init=$(realpath bosh-init/bosh-init-*)
+chmod +x ${bosh_init}
 
 echo "using bosh-init CLI version..."
-$initexe version
+${bosh_init} version
+
+function finish {
+  echo "Final state of director deployment:"
+  echo "=========================================="
+  cat "${output_dir}/director-state.json"
+  echo "=========================================="
+
+  cp -r $HOME/.bosh_init ${output_dir}
+}
+trap finish EXIT
 
 echo "upgrading existing BOSH Director VM..."
-time $initexe deploy director.yml
-
-time cp ${director_config}/director.env ${output_dir}
-time cp director{.yml,-state.json} ${output_dir}
-time cp -r $HOME/.bosh_init ${output_dir}
+pushd ${output_dir} > /dev/null
+  time ${bosh_init} deploy director.yml
+popd > /dev/null
 
 time bosh -n target ${BOSH_DIRECTOR_IP}
 time bosh login ${BOSH_DIRECTOR_USERNAME} ${BOSH_DIRECTOR_PASSWORD}
