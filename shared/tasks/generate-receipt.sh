05234a1e5253239df9044dd15890ec19ee968d6a
Corey Innis <cinnis@pivotal.io>
2016-05-04 14:27:07 -0700

    Fixes receipt filename to be unique across stemcells.
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- shared/tasks/generate-receipt.sh
+++ shared/tasks/generate-receipt.sh
@@ -10,7 +10,7 @@ CPI_RELEASE_VERSION=$(cat cpi-release/version)
 STEMCELL_VERSION=$(cat stemcell/version)
 
 timestamp=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-contents_hash=$(echo bosh-${BOSH_RELEASE_VERSION}-${CPI_RELEASE_NAME}-${CPI_RELEASE_VERSION}-${stemcell_name}-${STEMCELL_VERSION} | md5sum | cut -f1 -d ' ')
+contents_hash=$(echo bosh-${BOSH_RELEASE_VERSION}-${CPI_RELEASE_NAME}-${CPI_RELEASE_VERSION}-${STEMCELL_NAME}-${STEMCELL_VERSION} | md5sum | cut -f1 -d ' ')
 
 certify-artifacts                                   \
   --release bosh/$BOSH_RELEASE_VERSION              \
