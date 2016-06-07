c0baa605dd07f8d1a19aa769f6dbfb4e2d5595df
Forrest Sill <fsill@pivotal.io>
2016-06-07 08:27:57 -0700

    Ensure that cloud formation read errors cause script to exit
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- aws/utils.sh
+++ aws/utils.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -o pipefail
+set -e -o pipefail
 
 stack_data() {
   echo "$(aws cloudformation describe-stacks)" | \
-  jq --arg stack_name ${AWS_STACK_NAME} '.Stacks[] | select(.StackName=="\($stack_name)")'
+  jq -e --arg stack_name ${AWS_STACK_NAME} '.Stacks[] | select(.StackName=="\($stack_name)")'
 }
 
 stack_info() {
@@ -13,9 +13,9 @@ stack_info() {
   fi
 
   local key="$1"
-  echo "${AWS_STACK_INFO}" | jq -r --arg key ${key} '.Outputs[] | select(.OutputKey=="\($key)").OutputValue'
+  echo "${AWS_STACK_INFO}" | jq -e -r --arg key ${key} '.Outputs[] | select(.OutputKey=="\($key)").OutputValue'
 }
 
 stack_status() {
-  echo "$(stack_data)" | jq -r '.StackStatus'
+  echo "$(stack_data)" | jq -e -r '.StackStatus'
 }
