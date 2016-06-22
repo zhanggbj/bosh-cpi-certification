193534afa6b2a21128661a80549db53a4e216cfa
Lyle Franklin <lfranklin@pivotal.io>
2016-06-22 14:19:59 -0700

    Use timestamp as semver in s3-resource
    - Newest version of s3-resource requires a valid semver
    
    [#122080241](https://www.pivotaltracker.com/story/show/122080241)
    
    Signed-off-by: Forrest Sill <fsill@pivotal.io>

--- shared/tasks/generate-receipt.sh
+++ shared/tasks/generate-receipt.sh
@@ -9,11 +9,11 @@ BOSH_RELEASE_VERSION=$(cat bosh-release/version)
 CPI_RELEASE_VERSION=$(cat cpi-release/version)
 STEMCELL_VERSION=$(cat stemcell/version)
 
-timestamp=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+timestamp="$(date -u +%Y%m%d%H%M%S)"
 contents_hash=$(echo bosh-${BOSH_RELEASE_VERSION}-${CPI_RELEASE_NAME}-${CPI_RELEASE_VERSION}-${STEMCELL_NAME}-${STEMCELL_VERSION} | md5sum | cut -f1 -d ' ')
 
 certify-artifacts                                   \
   --release bosh/$BOSH_RELEASE_VERSION              \
   --release $CPI_RELEASE_NAME/$CPI_RELEASE_VERSION  \
   --stemcell $STEMCELL_NAME/$STEMCELL_VERSION       \
-  > certification/${timestamp}-${contents_hash}-receipt.json
+  > certification/${contents_hash}-${timestamp}-receipt.json
